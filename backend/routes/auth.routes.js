def59af6d9f3d0750a8a77c335a51c8bd8e1dae2
AG349 <138812063+AG349@users.noreply.github.com>
2026-03-03 23:22:14 +0530

    Delete backend directory

--- backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../controllers/auth.controller");
-const { requireAuth } = require("../middleware/auth.middleware");
-
-router.post("/login", auth.login);
-router.get("/me", requireAuth, auth.me);
-
-module.exports = router;
